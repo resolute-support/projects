d2b20be20c07ec4c76db27034edcb1566d9db6a9
cable <cableproxymaharaj@gmail.com>
2022-06-17 08:35:01 +0200

    added LCD and color sensor code

---
+++ Novice/Novice_color_sensor/colorview_arduino/colorview.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup() {
+  Serial.begin(9600);
+  //Serial.println("Color View Test!");
+
+  if (tcs.begin()) {
+    //Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+
+}
+
+
+void loop() {
+  float red, green, blue;
+  
+  tcs.setInterrupt(false);  // turn on LED
+  delay(50);  // takes 50ms to read
+  tcs.getRGB(&red, &green, &blue);
+
+  Serial.print("R:\t"); Serial.print(int(red)); 
+  Serial.print("\tG:\t"); Serial.print(int(green)); 
+  Serial.print("\tB:\t"); Serial.print(int(blue));
+  Serial.print("\n");
+}
